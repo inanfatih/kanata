790619b61d011abbadfac1427079f240dc5ac2ba
Fatih <mfatihinan@gmail.com>
2020-04-29 00:36:47 -0400

    bug fixes and enhancements

--- functions/handlers/contactUs.js
+++ functions/handlers/contactUs.js
@@ -1,32 +1,30 @@
 const { db } = require('../util/admin');
 
-const isEmail = (email) => {
-  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (email.match(regEx)) return true;
-  else return false;
-};
-
 exports.contactUs = (req, res) => {
   if (req.body.message.trim() === '') {
-    return res.status(400).json({ message: 'message must not be empty' });
+    return res.status(400).json({ message: 'Please enter your message' });
   }
 
   if (req.body.name.trim() === '') {
-    return res.status(400).json({ name: 'name must not be empty' });
+    return res.status(400).json({ name: 'Please enter your name' });
+  }
+
+  if (req.body.phone.trim() === '') {
+    return res.status(400).json({ phone: 'Please enter your phone' });
   }
 
   const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   if (!req.body.email.match(regEx)) {
-    return res.status(400).json({ email: 'email is not valid' });
+    return res.status(400).json({ email: 'Email is not valid' });
   }
 
   const newMessage = {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    message: req.body.message,
     createdAt: new Date().toISOString(),
-    name: req.user.name.trim(),
-    email: req.user.email.trim(),
-    phone: req.user.phone.trim(),
-    message: req.user.message.trim(),
   };
 
   db.collection('contactUsMessages')
